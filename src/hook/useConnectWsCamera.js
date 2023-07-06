bf309a037147d900af5d50c09566176a1aa8daf5
tunght1994 <tunght994@gmail.com>
2023-07-06 11:14:19 +0700

    update

--- src/hook/useConnectWsCamera.js
+++ src/hook/useConnectWsCamera.js
@@ -39,60 +39,60 @@ const useConnectWsCamera = () => {
     };
     wsRef.current.onmessage = (e) => {
       try {
-                const evtData = JSON.parse(e.data);
-                const { type } = evtData;
-                switch (type) {
-                    case STREAM: {
-                        setCameraStreamUrl(`data:image/png;base64,${evtData?.frame}`);
-                        break;
-                    }
-                    case COUNT: {
-                        setCount(evtData?.count);
-                        break;
-                    }
-                    case MESSAGE: {
-                        const { code } = evtData;
-                        let message = ""
-                        if (code === 20) {
-                            message = "Đang xử lý, vui lòng giữ yên khuôn mặt.";
-                        } else if (code === 21) {
-                            message = "Góc mặt không hợp lệ, vui lòng nhìn thẳng vào camera.";
-                        } else if (code === 22) {
-                            message =
-                                "Khuôn mặt không đủ chi tiết vui lòng di chuyển khuôn mặt vào vùng nhận diện.";
-                        } else if (code === 23) {
-                            message = "Khuôn mặt không hợp lệ.";
-                        } else if (code === 24) {
-                            message = "Khuôn mặt quá nhỏ, vui lòng đưa khuôn mặt gần hơn.";
-                        } else if (code === 25) {
-                            message = "Vui lòng giữ yên khuôn mặt trong vùng nhận diện.";
-                        } else {
-                            message = "Lấy khuôn mặt thành công.";
-                        }
-                        setMessage(message)
-                        break;
-                    }
-                    case ERROR: {
-                        const { code } = evtData;
-                        if (code === 11){
-                            setErrDevice("Không thấy camera kết nối")
-                        } else if (code === 12){
-                            setErrDevice("kết nối camera thất bại")
-                        }
-                        break;
-                    }
-                    case DATA: {
-                        stopCamera()
-                        setMessage("Lấy khuôn mặt thành công.")
-                        const {face} = evtData.data
-                        // dispatch(setFace(face))
-                    }
-                }
-            } catch (err) {
-                console.log("cannot parse data", e.data, err);
+        const evtData = JSON.parse(e.data);
+        const { type } = evtData;
+        switch (type) {
+          case STREAM: {
+            setCameraStreamUrl(`data:image/png;base64,${evtData?.frame}`);
+            break;
+          }
+          case COUNT: {
+            setCount(evtData?.count);
+            break;
+          }
+          case MESSAGE: {
+            const { code } = evtData;
+            let message = "";
+            if (code === 20) {
+              message = "Đang xử lý, vui lòng giữ yên khuôn mặt.";
+            } else if (code === 21) {
+              message = "Góc mặt không hợp lệ, vui lòng nhìn thẳng vào camera.";
+            } else if (code === 22) {
+              message =
+                "Khuôn mặt không đủ chi tiết vui lòng di chuyển khuôn mặt vào vùng nhận diện.";
+            } else if (code === 23) {
+              message = "Khuôn mặt không hợp lệ.";
+            } else if (code === 24) {
+              message = "Khuôn mặt quá nhỏ, vui lòng đưa khuôn mặt gần hơn.";
+            } else if (code === 25) {
+              message = "Vui lòng giữ yên khuôn mặt trong vùng nhận diện.";
+            } else {
+              message = "Lấy khuôn mặt thành công.";
             }
+            setMessage(message);
+            break;
+          }
+          case ERROR: {
+            const { code } = evtData;
+            if (code === 11) {
+              setErrDevice("Không thấy camera kết nối");
+            } else if (code === 12) {
+              setErrDevice("kết nối camera thất bại");
+            }
+            break;
+          }
+          case DATA: {
+            stopCamera();
+            setMessage("Lấy khuôn mặt thành công.");
+            const { face } = evtData.data;
+            // dispatch(setFace(face))
+          }
+        }
+      } catch (err) {
+        console.log("cannot parse data", e.data, err);
+      }
     };
-    wsRef.current.close = () => {
+    wsRef.current.onclose = () => {
       setWsConnected(false);
       if (isUnmounted.current) return;
       timerReconnect.current = setTimeout(connectWs, 1000);
@@ -105,20 +105,20 @@ const useConnectWsCamera = () => {
   useEffect(() => {
     connectWs();
     return () => {
-        isUnmounted.current = true;
-        clearTimeout(timerReconnect.current);
-        wsRef.current && wsConnected && stopCamera();
-        wsRef.current && wsRef.current.close();
-        setWsConnected(false);
-      };
+      isUnmounted.current = true;
+      clearTimeout(timerReconnect.current);
+      wsRef.current && wsConnected && stopCamera();
+      wsRef.current && wsRef.current.close();
+      setWsConnected(false);
+    };
   }, []);
 
   return {
     cameraStreamUrl,
     count,
     errDevice,
-    message
-  }
-}
+    message,
+  };
+};
 
 export default useConnectWsCamera;
